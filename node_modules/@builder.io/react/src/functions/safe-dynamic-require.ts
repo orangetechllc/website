7ade2230faf0588323d8c8a19fed178e312884b6
Nathan Hunt <nathan@orangetechllc.com>
2022-05-08 14:15:38 -0500

    initial commit

--- node_modules/@builder.io/react/src/functions/safe-dynamic-require.ts
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-import { Builder } from '@builder.io/sdk';
-
-// Allow us to require things dynamically safe from webpack bundling
-export const safeDynamicRequire: typeof require = Builder.isServer
-  ? eval('require')
-  : ((() => null) as any);
